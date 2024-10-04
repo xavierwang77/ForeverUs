950fff212f30fc68d01a13cd56e8583071ce58cc
Xavier Wang <xavierwang11@outlook.com>
2024-10-04 17:45:41 +0800

    add some features

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Img from 'next/image'
+import PageTransition from "@/components/PageTransition";
 import './globals.css'
+import React from "react";
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,14 +22,20 @@ export default function RootLayout({ children }: {
         <div className="flex flex-col min-h-screen">
             <header className="bg-primary text-primary-foreground py-2">
                 <div className="container mx-auto px-auto">
-                    <div className="flex items-center">
-                        <h1 className="text-2xl font-bold">Forever Us</h1>
-                        <Img src="/header-icon.svg" alt="Header Icon" width={24} height={24} className="ml-2 w-6 h-6"/>
+                    <div className="flex items-center justify-center">
+                        <Link href="/" passHref>
+                            <h1 className="text-2xl font-bold">Forever Us</h1>
+                        </Link>
+                        <Img src="/header-icon.svg" alt="Header Icon" width={24} height={24}
+                             className="ml-2 w-6 h-6"/>
                     </div>
                 </div>
             </header>
-            <main className="flex-grow flex flex-col items-center justify-center bg-gradient-to-b from-pink-100 to-purple-200 p-4 text-center">
-                {children}
+            <main
+                className="flex-grow flex flex-col items-center justify-center bg-gradient-to-b from-pink-100 to-purple-200 p-4 text-center relative">
+                <PageTransition>
+                    {children}
+                </PageTransition>
             </main>
             <footer className="bg-purple-200 py-2">
                 <div className="container mx-auto px-4 text-center text-sm text-gray-600">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from "@/components/ui/button"
-import Fireworks from "@/components/Fireworks"
 import ConfettiComponent from "@/components/Confetti"
 import Link from 'next/link';
+import Fireworks from "@/components/Fireworks";
 
 export default function Home() {
     const [timeElapsed, setTimeElapsed] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 })
@@ -34,13 +34,17 @@ export default function Home() {
 
     return (
         <div className="flex flex-col items-center justify-center">
-            <Fireworks />
-            <ConfettiComponent />
-            <h1 className="text-4xl font-bold text-purple-800 mb-8">M&X已经在一起...</h1>
-            <div className="text-9xl font-bold text-pink-600 mb-4 drop-shadow-lg">
-                {timeElapsed.days}
+            <ConfettiComponent/>
+            <div className="absolute inset-0">
+                <Fireworks/>
             </div>
-            <div className="text-4xl font-semibold text-purple-700 mb-8">天</div>
+            <h2 className="text-4xl font-bold text-purple-800 mb-8">M&X已经在一起</h2>
+            <div className="bg-white rounded-3xl shadow-2xl p-8 mb-8 bg-opacity-20">
+                <div className="text-10xl font-bold text-pink-600 mb-6 drop-shadow-lg animate-pulse leading-none">
+                    {timeElapsed.days}
+                </div>
+                <div className="text-4xl font-semibold text-purple-700">天</div>
+            </div>
             <div className="flex justify-center items-center space-x-4 text-4xl font-mono font-bold text-gray-800 mb-4">
                 <span>{formatNumber(timeElapsed.hours)}</span>
                 <span className="text-pink-500">:</span>
@@ -48,14 +52,18 @@ export default function Home() {
                 <span className="text-pink-500">:</span>
                 <span>{formatNumber(timeElapsed.seconds)}</span>
             </div>
-            <div className="flex justify-center items-center space-x-4 text-lg text-gray-700 mb-28">
+            <div className="flex justify-center items-center space-x-4 text-lg text-gray-700 mb-8">
                 <span className="w-16 text-center">时</span>
                 <span className="w-20 text-center">分</span>
                 <span className="w-16 text-center">秒</span>
             </div>
-            <Link href="/memories/m0">
-                <Button className="bg-purple-600 hover:bg-purple-700 text-white font-bold w-70 h-12 py-4 px-10 rounded-full text-2xl transition-all duration-200 ease-in-out transform hover:scale-110 shadow-lg">
-                    开启旅程
+            <div style={{height: '6rem'}}></div>
+            <Link href="/timeline/0" passHref>
+                <Button
+                    className="bg-purple-600 hover:bg-purple-700 text-white font-bold w-[17rem] h-18 py-4 px-8 rounded-full
+                    text-3xl text-center transition-all duration-200 ease-in-out transform hover:scale-110 shadow-lg"
+                >
+                    <span style={{letterSpacing: '0.8em', marginRight: '-0.8em'}}>开启旅程</span>
                 </Button>
             </Link>
         </div>
